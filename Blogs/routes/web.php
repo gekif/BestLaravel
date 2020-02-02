216ad39eb89f8915ccdd1cf8ae0101e97f3bb47b
Gekif <dzulfikar.maulana@gmail.com>
2020-02-02 20:45:40 +0700

    04 Project Two - Complete Blogging Content Management System: 023 Creating the post view

--- Blogs/routes/web.php
+++ Blogs/routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/post/creaete', [
+    'uses' => 'PostsController@create',
+    'as' => 'post.create'
+]);
