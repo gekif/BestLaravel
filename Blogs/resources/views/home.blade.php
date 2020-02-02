23f2c82056074256a38d7796601cd853effa4c05
Gekif <dzulfikar.maulana@gmail.com>
2020-02-02 21:07:28 +0700

    04 Project Two - Complete Blogging Content Management System: 025 Route groups and middleware

--- Blogs/resources/views/home.blade.php
+++ Blogs/resources/views/home.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 
 <div class="panel panel-default">
-
+    <div class="panel-heading">Dashboard</div>
+    <div class="panel-body">
+        Welcome!
+    </div>
 </div>
 
 @endsection
--- Blogs/routes/web.php
+++ Blogs/routes/web.php
@@ -15,16 +15,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
+
 
 Route::get('/home', 'HomeController@index');
 
-Route::get('/post/create', [
-    'uses' => 'PostsController@create',
-    'as' => 'post.create'
-]);
 
-Route::post('/post/store', [
-    'uses' => 'PostsController@store',
-    'as' => 'post.store'
-]);
+Route::group([
+    'prefix' => 'admin',
+    'middleware' => 'auth'
+], function () {
+
+    Route::get('/post/create', [
+        'uses' => 'PostsController@create',
+        'as' => 'post.create'
+    ]);
+
+    Route::post('/post/store', [
+        'uses' => 'PostsController@store',
+        'as' => 'post.store'
+    ]);
+
+});
+
+
