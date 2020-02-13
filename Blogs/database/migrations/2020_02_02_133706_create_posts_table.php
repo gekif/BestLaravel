f5bede6b39d94b6fd5513d25e3657c2b4957dfac
Gekif <dzulfikar.maulana@gmail.com>
2020-02-13 09:17:58 +0700

    04 Project Two - Complete Blogging Content Management System: 034 Soft deletes table setup

--- Blogs/database/migrations/2020_02_02_133706_create_posts_table.php
+++ Blogs/database/migrations/2020_02_02_133706_create_posts_table.php
@@ -15,10 +15,15 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+
             $table->string('title');
+            $table->string('slug');
             $table->string('content');
             $table->integer('category_id');
             $table->string('featured');
+
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
