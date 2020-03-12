c413109f1caab6d9410f542108385e81b146930d
Gekif <dzulfikar.maulana@gmail.com>
2020-03-12 21:28:05 +0700

    04 Project Two - Complete Blogging Content Management System: 050 More on relationships

--- Blogs/app/Http/Controllers/PostsController.php
+++ Blogs/app/Http/Controllers/PostsController.php
@@ -102,7 +102,8 @@ class PostsController extends Controller
 
         return view('admin.posts.edit')
             ->with('post', $post)
-            ->with('categories', Category::all());
+            ->with('categories', Category::all())
+            ->with('tags', Tag::all());
     }
 
     /**
@@ -137,6 +138,8 @@ class PostsController extends Controller
         $post->category_id = $request->input('category_id');
 
         $post->save();
+
+        $post->tags()->sync($request->tags);
 
         Session::flash('success', 'Post updated successfully');
 
--- Blogs/app/Post.php
+++ Blogs/app/Post.php
@@ -24,7 +24,7 @@ class Post extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo('App\Category');
     }
 
 
