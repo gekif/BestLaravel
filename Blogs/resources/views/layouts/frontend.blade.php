addfe8fe85a2fe40d6738d73536e00e7df45863b
Gekif <dzulfikar.maulana@gmail.com>
2020-05-09 19:19:51 +0700

    04 Project Two - Complete Blogging Content Management System: 072 AddThis installation into Blog

--- Blogs/resources/views/layouts/frontend.blade.php
+++ Blogs/resources/views/layouts/frontend.blade.php
@@ -155,6 +155,9 @@
 <script src="/app/js/ScrollMagic.min.js"></script>
 <script src="/app/js/animation.velocity.min.js"></script>
 
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5eb69e4270c4a2b0"></script>
+
 
 <!-- ...end JS Script -->
 
